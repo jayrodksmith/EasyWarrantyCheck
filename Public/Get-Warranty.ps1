7bb54069e9af813e717d4066b9c55af35f399052
David Brooks <david.brooks@outlook.com.au>
2024-02-16 16:50:52 +1100

    Server/central mode added, HP SystemSku manual input added

--- Public/Get-Warranty.ps1
+++ Public/Get-Warranty.ps1
@@ -17,100 +17,132 @@ function Get-Warranty {
         Manually set Manufacturer
     
     #>
-        [CmdletBinding(SupportsShouldProcess)]
-        param(
-            # RMM Mode
-            [Parameter(Mandatory = $false)]
-            [ValidateSet('NinjaRMM', 'None')]
-            [String]$RMM = 'NinjaRMM',
-            #Enable Registry Storing
-            [Parameter(Mandatory = $false)]
-            [bool]$EnableRegistry = $true,
-            [Parameter(Mandatory = $false)]
-            [String]$RegistryPath = 'HKLM:\SOFTWARE\RMMCustomInfo\',
-            # Force Update RMM with details
-            [Parameter(Mandatory = $false)]
-            [bool]$ForceUpdate = $false,
-            # Custom Machine Details
-            [Parameter(Mandatory = $false)]
-            [String]$Serial = 'Automatic',
-            [Parameter(Mandatory = $false)]
-            [String]$Manufacturer = 'Automatic',
-            # Set Date formats
-            [Parameter(Mandatory = $false)]
-            [String]$DateFormat = 'dd-MM-yyyy',
-            #NinjaRMM Custom Field Names
-            [Parameter(Mandatory = $false)]
-            [String]$ninjawarrantystart = 'warrantyStart',
-            [Parameter(Mandatory = $false)]
-            [String]$ninjawarrantyexpiry= 'warrantyExpiry',
-            [Parameter(Mandatory = $false)]
-            [String]$ninjawarrantystatus = 'warrantystatus',
-            [Parameter(Mandatory = $false)]
-            [String]$ninjainvoicenumber = 'invoicenumber'
+    [CmdletBinding(SupportsShouldProcess, DefaultParameterSetName = 'Default')]
+    Param(
+        # RMM Mode, available in both Default and CentralNinja sets
+        [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
+        [ValidateSet('NinjaRMM', 'None')]
+        [String]$RMM = 'NinjaRMM',
+    
+        # ServerMode, exclusive to CentralNinja but included in Default for consistency
+        [Parameter(Mandatory = $false, ParameterSetName = 'CentralNinja')]
+        [Switch]$ServerMode,
+    
+        # Enable Registry Storing
+        [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
+        [bool]$EnableRegistry = $true,
+    
+        # Registry Path
+        [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
+        [String]$RegistryPath = 'HKLM:\SOFTWARE\RMMCustomInfo\',
+    
+        # Force Update RMM with details
+        [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
+        [bool]$ForceUpdate = $false,
+    
+        # Custom Machine Details, available in both sets
+        [Parameter(Mandatory = $false, ParameterSetName = 'CentralNinja')]
+        [String]$Serial = 'Automatic',
+    
+        [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'CentralNinja')]
+        [String]$Manufacturer = 'Automatic',
+    
+        # Set Date formats, available in both sets
+        [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'CentralNinja')]
+        [String]$DateFormat = 'dd-MM-yyyy',
+    
+        # NinjaRMM Custom Field Names, available in both sets
+        [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'CentralNinja')]
+        [String]$ninjawarrantystart = 'warrantyStart',
+    
+        [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'CentralNinja')]
+        [String]$ninjawarrantyexpiry = 'warrantyExpiry',
+    
+        [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'CentralNinja')]
+        [String]$ninjawarrantystatus = 'warrantystatus',
+    
+        [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'CentralNinja')]
+        [String]$ninjainvoicenumber = 'invoicenumber',
 
-        )
-        # Set Global Variables
-        if($RMM -eq 'NinjaRMM'){
+        [Parameter(Mandatory = $false, ParameterSetName = 'CentralNinja')]
+        [String]$HpSystemSKU
+            
+    )
+    # Set Global Variables
+    if ($RMM -eq 'NinjaRMM') {
         Set-Variable ninjawarrantystart -Value $ninjawarrantystart -Scope Global -option ReadOnly -Force
         Set-Variable ninjawarrantyexpiry -Value $ninjawarrantyexpiry -Scope Global -option ReadOnly -Force
         Set-Variable ninjawarrantystatus -Value $ninjawarrantystatus -Scope Global -option ReadOnly -Force
         Set-Variable ninjainvoicenumber -Value $ninjainvoicenumber -Scope Global -option ReadOnly -Force
-        }
-        if($ForceUpdate -eq $true){
+    }
+    if ($ForceUpdate -eq $true) {
         Set-Variable ForceUpdate -Value $ForceUpdate -Scope Global -option ReadOnly -Force
-        }
+    }
+    if ($PSCmdlet.ParameterSetName -eq 'Default') {
         $machineinfo = Get-MachineInfo
-        if($serial -eq 'Automatic'){
+        if ($serial -eq 'Automatic') {
             $serialnumber = $machineinfo.serialnumber
-        } else {
+        }
+        else {
             $serialnumber = $serial
         }
-        if($Manufacturer -eq 'Automatic'){
+        if ($Manufacturer -eq 'Automatic') {
             $mfg = $machineinfo.Manufacturer
-        } else {
+        }
+        else {
             $mfg = $Manufacturer
         }
+    } else {
+        $serialnumber = $serial
+        $mfg = $Manufacturer
+    }
         
-        $Notsupported = $false
-        switch -Wildcard ($mfg){
-            "EDSYS"{
-                $Warobj = Get-WarrantyEdsys -Serial $serialnumber -DateFormat $DateFormat
+    $Notsupported = $false
+    switch -Wildcard ($mfg) {
+        "EDSYS" {
+            $Warobj = Get-WarrantyEdsys -Serial $serialnumber -DateFormat $DateFormat
+        }
+        "ASUS" {
+            $Warobj = Get-WarrantyAsus -Serial $serialnumber -DateFormat $DateFormat
+        }
+        "LENOVO" {
+            $Warobj = Get-WarrantyLenovo -Serial $serialnumber -DateFormat $DateFormat
+        }
+        "DELL" {
+            $Warobj = Get-WarrantyDell -Serial $serialnumber -DateFormat $DateFormat
+        }
+        "HP" {
+            $Warobj = Get-WarrantyHP -Serial $serialnumber -DateFormat $DateFormat
+        }
+        "MICROSOFT" {
+            if ($($machineinfo.Model) -like 'SurfaceNotSupportedYet') {
+                $Warobj = Get-WarrantyMicrosoft -Serial $serialnumber -DateFormat $DateFormat
             }
-            "ASUS"{
-                $Warobj = Get-WarrantyAsus -Serial $serialnumber -DateFormat $DateFormat
-            }
-            "LENOVO"{
-                $Warobj = Get-WarrantyLenovo -Serial $serialnumber -DateFormat $DateFormat
-            }
-            "DELL"{
-                $Warobj = Get-WarrantyDell -Serial $serialnumber -DateFormat $DateFormat
-            }
-            "HP"{
-                $Warobj = Get-WarrantyHP -Serial $serialnumber -DateFormat $DateFormat
-            }
-            "MICROSOFT"{
-                if($($machineinfo.Model) -like 'SurfaceNotSupportedYet'){
-                    $Warobj = Get-WarrantyMicrosoft -Serial $serialnumber -DateFormat $DateFormat
-                } else{
-                    $Notsupported = $true
-                    Write-Host "Microsoft Model not Supported"
-                    Write-Host "Manufacturer  :  $mfg"
-                    Write-Host "Model         :  $($machineinfo.Model)"
-                }
-                
-            }
-            "TOSHIBA"{
-                $Warobj = Get-WarrantyToshiba -Serial $serialnumber -DateFormat $DateFormat
-            }
-            default{
+            else {
                 $Notsupported = $true
-                Write-Host "Manufacturer or Model not Supported"
+                Write-Host "Microsoft Model not Supported"
                 Write-Host "Manufacturer  :  $mfg"
                 Write-Host "Model         :  $($machineinfo.Model)"
             }
+                
         }
-    if($RMM -eq 'NinjaRMM' -and ($Notsupported -eq $false)){
+        "TOSHIBA" {
+            $Warobj = Get-WarrantyToshiba -Serial $serialnumber -DateFormat $DateFormat
+        }
+        default {
+            $Notsupported = $true
+            Write-Host "Manufacturer or Model not Supported"
+            Write-Host "Manufacturer  :  $mfg"
+            Write-Host "Model         :  $($machineinfo.Model)"
+        }
+    }
+    if ($RMM -eq 'NinjaRMM' -and ($Notsupported -eq $false) -and !$ServerMode.IsPresent) {
         $ParamsNinjaRMM = @{
             DateFormat = $DateFormat
         }
@@ -128,7 +160,7 @@ function Get-Warranty {
         }
         Write-WarrantyNinjaRMM @ParamsNinjaRMM
     }
-    if($EnableRegistry -and ($Notsupported -eq $false)){
+    if ($EnableRegistry -and ($Notsupported -eq $false) -and !$ServerMode.IsPresent) {
         $Params = @{}
         if ($WarObj.'StartDate') {
             $Params['Warrantystart'] = $WarObj.'StartDate'
@@ -144,5 +176,5 @@ function Get-Warranty {
         }
         Write-WarrantyRegistry -RegistryPath $RegistryPath @Params
     }
-return $Warobj
+    return $Warobj
 }
