228da72361219b065e6b7f14e366e67aa61a164c
jayrodksmith <51105538+jayrodksmith@users.noreply.github.com>
2024-03-21 13:08:46 +0800

    Added check for Ninja fields

--- Private/RMM/Get-WarrantyNinjaRMM.ps1
+++ Private/RMM/Get-WarrantyNinjaRMM.ps1
@@ -18,7 +18,27 @@ function Get-WarrantyNinjaRMM {
             [Parameter(Mandatory = $false)]
             [Switch]$Display
         )
-        
+        # Test if fields exist
+        $testninjawarrantystart = Ninja-Property-Get $ninjawarrantystart 2>&1
+        if ($testninjawarrantystart -match "Unable to find the specified field" ){
+            Write-Host "Unable to access warrantystart field in ninja"
+            Write-Host "Check permissions of field and that it exists"
+        }
+        $testninjawarrantystatus = Ninja-Property-Get $ninjawarrantystatus 2>&1
+        if ($testninjawarrantystatus -match "Unable to find the specified field" ){
+            Write-Host "Unable to access warrantystatus field in ninja"
+            Write-Host "Check permissions of field and that it exists"
+        }
+        $testninjawarrantyexpiry = Ninja-Property-Get $ninjawarrantyexpiry 2>&1
+        if ($testninjawarrantyexpiry -match "Unable to find the specified field" ){
+            Write-Host "Unable to access warrantyexpiry field in ninja"
+            Write-Host "Check permissions of field and that it exists"
+        }
+        $testninjainvoicenumber  = Ninja-Property-Get $ninjainvoicenumber 2>&1
+        if ($testninjainvoicenumber  -match "Unable to find the specified field" ){
+            Write-Host "Unable to access invoicenumber field in ninja"
+            Write-Host "Check permissions of field and that it exists"
+        }
         $ninjawarrantystartvalue = Ninja-Property-Get $ninjawarrantystart
         $ninjawarrantystatusvalue = Ninja-Property-Get $ninjawarrantystatus
         $ninjawarrantyexpiryvalue = Ninja-Property-Get $ninjawarrantyexpiry
